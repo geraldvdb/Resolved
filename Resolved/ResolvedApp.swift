f299f462ef7e80b55b861efec71e30449c7f3aef
gvandenberg <gvandenberg@etsy.com>
2024-12-30 15:37:37 -0500

    Initial Commit

---
+++ Resolved/ResolvedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResolvedApp.swift
+//  Resolved
+//
+//  Created by Gerald Van Den Berg on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ResolvedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ResolvedTests/ResolvedTests.swift
@@ -0,0 +1,17 @@
+//
+//  ResolvedTests.swift
+//  ResolvedTests
+//
+//  Created by Gerald Van Den Berg on 12/30/24.
+//
+
+import Testing
+@testable import Resolved
+
+struct ResolvedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
